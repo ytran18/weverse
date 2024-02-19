c6b8d9f43e0ec3f15e97236addad8f3988940f9f
jin <a11718trannhuy@gmail.com>
2024-02-19 16:00:35 +0700

    update code

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
+import { SharedComponentsModule } from '../../components/shared-components/shared-components.module';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  imports: [SharedComponentsModule],
 })
 export class LoginComponent {
 
